70243a65ffcaecfa18fa88349aea096a664478f8
Chun <lee@kilimoagtech.com>
2019-08-29 07:31:48 -0300

    generic api views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,3 @@
-from rest_framework import mixins
 from rest_framework import generics
 
 from polls.models import Question
@@ -6,30 +5,11 @@ from polls.serializers import QuestionSerializerWithMS as QuestionSerializer
 
 # Create your views here.
 
-class QuestionList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class QuestionDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    
